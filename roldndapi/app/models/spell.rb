74ca529783611f3d1fb8a66e488d0a58c20161ee
silverfishseeker <83122444+silverfishseeker@users.noreply.github.com>
2024-06-09 10:36:07 +0200

    recolección de hechizos completa

--- roldndapi/app/models/spell.rb
+++ roldndapi/app/models/spell.rb
@@ -1,26 +1,32 @@
 class Spell < ApplicationRecord
 
-  # Spell.source_update
-
   def self.source_update 
     das = DndApiService.new
     allCurr = Spell.all.pluck(:name)
     das.fetch_spells.each do |name, index|
       if not allCurr.include?(name)
-        api_spell = das.fetch_spell(index)
+        sp = das.fetch_spell(index)
         Spell.create(
-          name: api_spell["name"],
-          url: api_spell["url"],
-          level: api_spell["level"], 
+          name: sp["name"],
+          url: sp["url"],
+          level: sp["level"], 
           clases:
-            api_spell["classes"].map{ |clase| clase["name"] }.join(", ") +
-            (api_spell["subclasses"].any? ?
-             ("  SUBCLASES: " +  api_spell["subclasses"].map{ |clase| clase["name"] }.join(", "))
+            sp["classes"].map{ |clase| clase["name"] }.join(", ") +
+            (sp["subclasses"].any? ?
+             ("  Subclases: " +  sp["subclasses"].map{ |clase| clase["name"] }.join(", "))
             : ""), 
-          desc: ""
+          desc: "#{sp["desc"].join(" ")} #{sp["higher_level"].join(" ")} "\
+            "Rango: #{sp["range"]}. Material: #{sp["material"]}. "\
+            "#{sp["area_of_effect"] ? "Área de efecto: #{sp["area_of_effect"]["type"]}, #{sp["size"]}. " : ""}"\
+            "#{sp["ritual"] ? "Ritual.": ""} #{sp["concentration"] ? "Concentración." : ""} "\
+            "Tiempo de lanzamiento: #{sp["casting_time"]}. Tipo de ataque: #{sp["attack_type"]}. "\
+            "Daño: #{sp["damage"]["damage_type"]["name"]}, #{sp["damage"]["damage_at_slot_level"]}}. "\
+            "Escuela: #{sp["school"]["name"]}"
         )
       end
     end
   end
+
+  Spell.source_update
 
 end
